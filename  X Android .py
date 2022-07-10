f31f8330d838d8762de54efe88fadeeacde86193
7-MAX25 <107028520+7-MAX25@users.noreply.github.com>
2022-07-10 11:52:08 +0200

    Add files via upload

---
+++  X Android .py
@@ -0,0 +1,57 @@
+import os, sys, time  
+def jalan (z):
+        for e in z +'\n':
+                sys.stdout.write(e)
+                sys.stdout.flush ()
+                time.sleep (00000.05)
+os.system ('clear')
+print ("\033[1;31m")
+jalan ('Welcome to my tool')
+time.sleep(2)
+os.system ("clear")
+print ("\033[1;32m") 
+jalan ("""
+┈┈┃┈▇┈┈▇┈┃
+╭╮┣━━━━━━┫╭╮
+┃┃┃┈┈┈┈┈┈┃┃┃
+╰╯┃┈┈┈┈┈┈┃╰╯
+┈┈╰┓┏━━┓┏╯
+┈┈┈╰╯┈┈╰╯""")
+time.sleep(1)
+
+jalan (''' █░█ ▄▀▄ █▄░█ █▀▄ █▀▀▄ ▄▀▄ ▀ █▀▄
+ ▄▀▄ █▀█ █░▀█ █░█ █▐█▀ █░█ █ █░█
+ ▀░▀ ▀░▀ ▀░░▀ ▀▀░ ▀░▀▀ ░▀░ ▀ ▀▀░''') 
+#########################
+print ("\033[1;34m")
+jalan ("To communicate with me") 
+print ("--------------------------------------------------------------------------------------------------------------") 
+jalan ("Telegram: https://t.me/REDSCORPIOIN") 
+jalan ("YouTube: https://youtube.com/channel/UCaJQuSLpT8-7l5VfY-d7h9A")
+print ("---------------------------------------------------------------------------------------------------------------") 
+#########################
+print ("\033[1;33m")
+jalan ("(1] Storm-Breaker") 
+jalan ("[2] Ddos404")
+jalan ("[3] Fetch the victim's IP") 
+jalan ("[4] hacker Social media") 
+ 
+list()
+print(" ")
+Choose = input(" [«] Choose :")
+os.system ("clear") 
+
+
+if Choose =='1':
+        os.system("https://github.com/ultrasecurity/Storm-Breaker;cd Storm-Breaker;sudo bash linux-installer.sh;python3 -m pip install -r requirments.txt;python3 Storm-Breaker.py") 
+
+
+	
+elif Choose =="2": 
+	os.system("git clone https://github.com/CyberMrlink/Ddos404;cd Ddos404;sh ddos.sh")
+	os.system("python spider X.py")
+
+elif Choose =="3": 
+	os.system("xdg-open https://grabify.link/") 
+elif Choose  =="4": 
+	os.system("xdg-open http://sp0m.com/en/register") 
